3fab47b3283fe796bb37efc9b57a7782a8733f78
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-09 20:16:55 +0100

    działa

--- MorseAlphabetConverter/MorseAlphabetConverter/MainWindow.xaml.cs
+++ MorseAlphabetConverter/MorseAlphabetConverter/MainWindow.xaml.cs
@@ -20,9 +20,86 @@ namespace MorseAlphabetConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Dictionary<char, string> translator;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            char dot = '.';
+            char dash = '−';
+
+            translator = new Dictionary<char, string>()
+            {
+                {'a', string.Concat(dot, dash)},
+                {'b', string.Concat(dash, dot, dot, dot)},
+                {'c', string.Concat(dash, dot, dash, dot)},
+                {'d', string.Concat(dash, dot, dot)},
+                {'e', dot.ToString()},
+                {'f', string.Concat(dot, dot, dash, dot)},
+                {'g', string.Concat(dash, dash, dot)},
+                {'h', string.Concat(dot, dot, dot, dot)},
+                {'i', string.Concat(dot, dot)},
+                {'j', string.Concat(dot, dash, dash, dash)},
+                {'k', string.Concat(dash, dot, dash)},
+                {'l', string.Concat(dot, dash, dot, dot)},
+                {'m', string.Concat(dash, dash)},
+                {'n', string.Concat(dash, dot)},
+                {'o', string.Concat(dash, dash, dash)},
+                {'p', string.Concat(dot, dash, dash, dot)},
+                {'q', string.Concat(dash, dash, dot, dash)},
+                {'r', string.Concat(dot, dash, dot)},
+                {'s', string.Concat(dot, dot, dot)},
+                {'t', string.Concat(dash)},
+                {'u', string.Concat(dot, dot, dash)},
+                {'v', string.Concat(dot, dot, dot, dash)},
+                {'w', string.Concat(dot, dash, dash)},
+                {'x', string.Concat(dash, dot, dot, dash)},
+                {'y', string.Concat(dash, dot, dash, dash)},
+                {'z', string.Concat(dash, dash, dot, dot)},
+                {'0', string.Concat(dash, dash, dash, dash, dash)},
+                {'1', string.Concat(dot, dash, dash, dash, dash)},
+                {'2', string.Concat(dot, dot, dash, dash, dash)},
+                {'3', string.Concat(dot, dot, dot, dash, dash)},
+                {'4', string.Concat(dot, dot, dot, dot, dash)},
+                {'5', string.Concat(dot, dot, dot, dot, dot)},
+                {'6', string.Concat(dash, dot, dot, dot, dot)},
+                {'7', string.Concat(dash, dash, dot, dot, dot)},
+                {'8', string.Concat(dash, dash, dash, dot, dot)},
+                {'9', string.Concat(dash, dash, dash, dash, dot)}
+            };
+            string input = textTextBox.Text;
+            outputTextBox.Text = translate(input);
+            
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Text = null;
+        }
+
+        private static string translate(string input)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (char character in input)
+            {
+                if (translator.ContainsKey(character))
+                {
+                    sb.Append(translator[character] + "  ");
+                }
+                else if (character == ' ')
+                {
+                    sb.Append("/ " + " ");
+                }
+                else
+                {
+                    sb.Append(character + "  ");
+                }
+            }
+            return sb.ToString();
         }
     }
 }
